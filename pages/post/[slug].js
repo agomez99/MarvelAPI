3d6954991c3ec7b65f4a0a3e420880e3712bd2df
Austine Gomez <austine.gomez@yahoo.com>
2021-02-21 20:59:09 -0600

    chart

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -4,57 +4,89 @@ import { getPostBySlug } from '@lib/firebase';
 import { Layout } from '@components';
 import { Col, Row, Card } from 'tailwind-react-ui'
 import Image from 'next/image';
+import { HorizontalBar } from 'react-chartjs-2';
 const PostPage = ({ post }) => {
   const router = useRouter();
- 
+
   if (!post && typeof window !== 'undefined') {
-      router.push('/404');
-      return;
+    router.push('/404');
+    return;
   }
 
   if (!post) {
-      return null;
+    return null;
   }
 
+  const state = {
 
-  return(
-  <Layout>
-    <div className={styles.PostPage}>
-    <Card style={{backgroundColor:"rgb(226, 235, 175)", padding:"10px"}}>
-    <Row>
-        <div style={{backgroundColor:"rgb(74, 147, 230)", width:"100%",margin:"0px 0px", marginBottom:"5%"}}>
-      <h1 style={{textAlign:"left", fontSize:"8vh", fontWeight:"bold", color:"white", textTransform:"upperCase"}}>{post.name}</h1>
-      </div>
-      </Row>
-      <Row>
-        <Col w="1/4" bg="grey-light" text="center" p="4">
-            {/* <img src={post.image} alt={post.imageAlt} /> */}
-            <img src={post.image2} alt={post.imageAlt}  />
+    labels: ['STRENGTH', 'SPEED', 'AGILITY',
+      'STAMINA', 'DURABILITY', 'INTELLIGENCE'],
+    datasets: [
+      {
+        label: 'POWER RATINGS',
+        backgroundColor: 'pink',
+        borderColor: 'black',
+        color: 'white',
+        borderWidth: 3,
+        data: [65, 59, 80, 81, 56, 50],
 
-        </Col>
-        <Col  w="1/2">
-        <Image
-          src="/chart.png"
-        alt="Picture of the author"
-        width={500}
-        height={500}></Image>
-        </Col>
-      </Row>
-      <Row>
-      <div  style={{backgroundColor:"white", padding:"20px"}}>
-        <p style={{fontSize:"1.5vh"}}> {post.bio }</p>
-      </div>
-      </Row>
+      }
+    ]
+  }
 
-      <Row>
-      <div  style={{backgroundColor:"rgb(226, 235, 175)", padding:"10px", borderWidth:"2px",borderColor:"black"}}>
-        <p style={{fontSize:"1.5vh", fontWeight:"bold"}}>DID YOU KNOW :</p><p> {post.dyn}</p>
+  return (
+    <Layout>
+      <div className={styles.PostPage}>
+        <Card style={{ backgroundColor: "rgb(226, 235, 175)", padding: "10px" }}>
+          <Row>
+            <div style={{ backgroundColor: "rgb(74, 147, 230)", width: "100%", margin: "0px 0px", marginBottom: "5%" }}>
+              <h1 style={{ textAlign: "left", fontSize: "8vw", fontWeight: "bold", color: "white", textTransform: "upperCase" }}>{post.name}</h1>
+            </div>
+          </Row>
+          <Row>
+            <Col w="1/4" bg="grey-light" text="center" p="4">
+              {/* <img src={post.image} alt={post.imageAlt} /> */}
+              <img src={post.image2} alt={post.imageAlt} />
+
+            </Col>
+            <Col w="1/2">
+              <HorizontalBar
+                data={state}
+                options={{
+                  title: {
+                    display: true,
+                    text: 'POWER RATINGS',
+                    fontSize: 40,
+                    fontColor: ' black',
+
+                  },
+                  legend: {
+                    display: true,
+                    position: 'bottom',
+
+                
+                  },
+                }
+                }
+
+              />
+            </Col>
+          </Row>
+          <Row>
+            <div style={{ backgroundColor: "white", padding: "20px" }}>
+              <p style={{ fontSize: "1.5vh" }}> {post.bio}</p>
+            </div>
+          </Row>
+
+          <Row>
+            <div style={{ backgroundColor: "rgb(226, 235, 175)", padding: "10px", borderWidth: "2px", borderColor: "black" }}>
+              <p style={{ fontSize: "1.5vh", fontWeight: "bold" }}>DID YOU KNOW :</p><p> {post.dyn}</p>
+            </div>
+          </Row>
+        </Card>
       </div>
-      </Row>
-      </Card>
-</div>
-  </Layout>
-);
+    </Layout>
+  );
 
 }
 export async function getServerSideProps(context) {
