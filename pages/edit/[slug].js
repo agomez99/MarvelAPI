b3bf66ef8f504d03309d278413366f1b5dda8897
Austine Gomez <austine.gomez@yahoo.com>
2021-04-19 22:42:58 -0500

    Update [slug].js

--- pages/edit/[slug].js
+++ pages/edit/[slug].js
@@ -187,7 +187,7 @@ const EditPage = ({ post }) => {
             />
           </div>
           </div>
-          <Button style={{backgroundColor:"green"}} type="submit" disabled={isLoading}>
+          <Button style={{backgroundColor:"green", marginBottom:"15px"}} type="submit" disabled={isLoading}>
             {isLoading ? 'Updating...' : 'Update'}
           </Button>
         </form>
