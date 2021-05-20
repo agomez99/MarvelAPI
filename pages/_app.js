d0ded01f89fe59e4aa4a370fc2d2870368dbbeca
Austine Gomez <austine.gomez@yahoo.com>
2021-05-20 18:16:18 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,11 @@
 import { AuthProvider } from '@contexts/auth';
 import '@styles/global.scss';
 import "tailwindcss/tailwind.css";
-import * as ga from '../lib/ga'
 
-function App ({ Component, pageProps })  {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-}
+const App = ({ Component, pageProps }) => (
   <AuthProvider>
     <Component {...pageProps} />
   </AuthProvider>
-
+);
 
 export default App;
