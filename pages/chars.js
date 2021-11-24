52a5613d19c18cebe6f9d868cd6f51ff5ba3ffa4
Austine Gomez <austine.gomez@yahoo.com>
2021-11-23 23:07:06 -0600

    css

--- pages/chars.js
+++ pages/chars.js
@@ -1,8 +1,8 @@
-import styles from "@styles/index.module.scss";
 import { getPosts } from "@lib/firebase";
 import { Layout } from "@components";
 import { Col, Row, Flex, Box } from "tailwind-react-ui";
 import Image from "next/image";
+import styles from "@styles/chars.module.scss";
 
 const Characters = ({ post }) => {
   if (!post && typeof window !== "undefined") {
@@ -29,51 +29,15 @@ const Characters = ({ post }) => {
 
   return (
     <div>
-      <Layout className={styles.HomePage}>
-        <div>
-          <div
-            style={{
-              backgroundColor: "rgba(0,0,0,0.5)",
-              width: "100%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <h1
-              style={{
-                color: "white",
-                fontSize: "5.5rem",
-                fontFamily: "Bangers",
-                textAlign: "center",
-                paddingRight: "15px",
-              }}
-            >
-              Cardverse
-            </h1>
+      <Layout>
+        <div className={styles.CharsPage}>
+          <div className={styles.title}>
+            <h1>Cardverse</h1>
             <Image src="/cardverse.png" alt="logo" width={300} height={300} />
           </div>
-          <h1
-            style={{
-              marginTop: "5%",
-              color: "white",
-              fontSize: "3.5rem",
-              fontFamily: "Bangers",
-              textAlign: "center",
-              backgroundColor: "rgba(1,0,0,1)",
-            }}
-          >
-            Characters Index
-          </h1>
+          <h1 className={styles.charindex}>Characters Index</h1>
           <div>
-            <Row
-              style={{
-                color: "white",
-                fontFamily: "Bangers",
-                textAlign: "left",
-                backgroundColor: "rgba(1,0,0,1)",
-              }}
-            >
+            <Row className={styles.charrow}>
               <Flex style={{ padding: "20px 50px 70px 20px" }}>
                 <Box p={4} bg="grey-light" flex={1} text="left">
                   <Col>
@@ -85,11 +49,9 @@ const Characters = ({ post }) => {
                           <a
                             href={`/post/${post.slug}`}
                             key={post.number.toString()}
-                            key={post.number.toString()}
                             className="indexChar"
                           >
                             {post.number} - {post.cardname}
-                            {/* {post.number} - {titleCase(post.cardname)} */}
                             <img
                               src={post.imageAlt}
                               alt={post.imageAlt}
